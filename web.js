ee811ff3396d6002fcc7a3f9d13d878cd83a6f9c
Nathan Sanford <nbsanford.dev@gmail.com>
2013-09-02 07:02:22 +0000

    first shot at buffers

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 app.use(express.logger());
+var fs = require('fs');
+var buf = new Buffer(256);
+
+fs.readSync('./index.html',buf,256,0);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
